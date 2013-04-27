df02a03c0a5fa7f22ce2f307fa0f3e7789a05ac9
Matthias Knoll <matthias@bravenewtalent.com>
2013-04-27 18:07:53 +0100

    added maps and start selection

--- client/js/app.js
+++ client/js/app.js
@@ -1,11 +1,31 @@
-goog.provide("app");
+goog.provide("App");
 
 goog.require("goog.dom");
+goog.require("goog.events.EventTarget");
 
 goog.scope(function(){
-    
-    goog.exportSymbol('app.start', function(){
-        console.log('start app');
-    });
+
+    var App = function () {
+        goog.base(this);
+    };
+
+    goog.inherits(App, goog.events.EventTarget);
+    goog.addSingletonGetter(App);
+
+    App.prototype.initialise = function () {
+        this.initialiseMap();
+    };
+
+    App.prototype.initialiseMap = function () {
+        var mapOptions = {
+                zoom: 8,
+                center: new google.maps.LatLng(-34.397, 150.644),
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            },
+            map = new google.maps.Map(goog.dom.getElementByClass('js-map-canvas'), mapOptions);
+
+    };
+
+    goog.exportSymbol('app.start', App.getInstance().initialise.bind(App.getInstance()));
 
 });
