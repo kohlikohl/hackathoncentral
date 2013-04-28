891ffac3cf0909822538799cdf5db302fb62e824
Gordon Murray Dent <gordon@bravenewtalent.com>
2013-04-28 13:59:12 +0100

    even more awesomeness

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,9 @@
-var Data = require("../library/data.js");
-var when = require('when');
+var Data = require("../library/data.js"),
+    when = require('when'),
+    redis = require('redis')
+    client = redis.createClient();
+
+    redis.debug_mode = true;
 
 var IndexController = module.exports = {};
 
@@ -34,7 +38,11 @@ IndexController.personas = function(req,res) {
 }
 
 IndexController.persona = function(req,res) {
-    var dataObject = new Data('1');
+    var dataObject = new Data('1'),
+    count = client.get('count', function(err,result) { if (err | !result) { count = 1; } });
+    client.setex("count",600,++count);
+
+    console.log(count);
     dataObject.getPersona(req.params.persona, req.params.borough).then(
         function done(data) {
             return data;
