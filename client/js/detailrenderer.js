989289d750a27f42b640917f555ff68c2f048d3b
Matthias Knoll <matthias@bravenewtalent.com>
2013-04-28 04:17:02 +0100

    display data

--- client/js/detailrenderer.js
+++ client/js/detailrenderer.js
@@ -1,13 +1,18 @@
 goog.provide("app.renderer.Detail");
 
+goog.require("goog.dom");
+
 goog.scope(function () {
     var _ = app.renderer;
 
     _.Detail = function () {
-
+        this.nameElement = goog.dom.getElementByClass('js-borough-name');
+        this.scoreElement = goog.dom.getElementByClass('js-borough-score');
     };
 
-    _.Detail.prototype.render = function () {
-        
+    _.Detail.prototype.render = function (data) {
+
+        goog.dom.setTextContent(this.nameElement, data.name);
+        goog.dom.setTextContent(this.scoreElement, Math.round(data.score * 100));
     };
 });
